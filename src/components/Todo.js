884965e101ea06c43513560a3f938a07f4c8ee20
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-09-13 22:31:53 +0430

    iscompleted style added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,12 +3,19 @@ function Todo({info, keys, completeTodo}) {
     return (
         <div className='todo-item' key={info.id}>
             <div>{keys+1}</div>
-            <div className="todo-text">
+            <div className={`todo-text ${info.isComplete ? "complete" : ""}`}>
                 {info.text}
             </div>
             <div className="buttons-container">
                 <button>Edit</button>
-                <button onClick={completeTodo}>Mark As Complete</button>
+                <button onClick={completeTodo}>
+                    {
+                        !info.isComplete ?
+                        <>Mark As Complete</>
+                        :
+                        <>Mark As Not Complete</>
+                    }
+                </button>
             </div>
         </div>)
 }
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -18,16 +18,22 @@ function TodoApp() {
     }
 
     const onCompleteTodo = (id) => {
+        //! these steps are importantّ
+
         //? we must first find the index we are looking for to change
         let selectedIndex = todos.findIndex((todo) => todo.id === id)
 
-        //* then we will change the 
-        let selectedTodo = {...todos[selectedIndex]}
+        //* then we will clone the wanted object form the original to not to mutate the whole thing
+        //* then we will change what ever we want to change from it
+        let selectedTodo = { ...todos[selectedIndex] }
         selectedTodo.isComplete = !selectedTodo.isComplete
 
+        //? then we will clone the whole thing again because we don't want to mutate it
+        //? and then change the index found with the wanted object
         let updateTodo = [...todos]
         updateTodo[selectedIndex] = selectedTodo
 
+        //* at last we set our list with the new value
         setTodos(updateTodo)
     }
 
@@ -36,7 +42,7 @@ function TodoApp() {
             <h2>TodoList</h2>
             <TodoForm addTodo={todoChangeHandler} />
             <hr />
-            <TodoList list={todos} onCompleteTodo={onCompleteTodo}/>
+            <TodoList list={todos} onCompleteTodo={onCompleteTodo} />
         </div>
     )
 }
