0b47b8d4d60af2ea97ee2a3b3e7e3388e9a3a954
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-09-13 15:18:21 +0430

    default project and styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoApp from './components/TodoApp.js';
+import "./styles/App.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
